a1ab5426a5e8e72059fe7b6632c0bc8253cc1155
Joe Hernandez <112195044+Nandez22@users.noreply.github.com>
2024-11-26 02:59:02 -0600

    Logging *should* work now
    There is a bug with the level logic which makes `JSONSerialsizer.Instance.SceneNumber` report two numbers (the second always being 7) each time a new level is loaded. I believe this is causing the logger to always write to 7.csv instead of the proper trial nums. (I just realized that it should be based off of trial num, not level num... But still this is a bug that has to be fixed for proper stats)

--- SharpHallway/Assets/Scripts/Logging/TransformSpy.cs
+++ SharpHallway/Assets/Scripts/Logging/TransformSpy.cs
@@ -4,56 +4,81 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Text;
 using System;
+using System.Buffers.Text;
+using System.Linq;
 
 public class TransformSpy : MonoBehaviour
 {
+    //Looking at this code has given me a small handful of diseases but at least it works :)  -- It's mostly the naming schemes and the orginization of the program (TransformSpy should not be the logging manager [hey thats a good file name!])
+
 	public static TransformSpy instance;
 	public TrialLogger trialLogger { get; private set; }
 
-	private string path;
+	private string basePath;
 
     public void Awake() {
 		instance = this;
 		if(gameObject == null){ throw new MissingReferenceException("TransformSpy: TransformSpy must be attached to player camera for logging to occur."); }
-
-		//TEMPORARY CHANGE ASAP
-		path = Path.Combine(Application.persistentDataPath, "tempLog.csv");
+        CreateBasePath();
 	}
-
-	//Whenever the expirement starts (IE Trial 1), create a directory inside persistant data path with a unique name (Maybe millisecond timecode put through simple cypher).
-	//Use that directory for the rest of the trials. Create a new one for each expirement (if that wasn't already apparent)
-
-	//The path would be something like "{Application.PersistantDataPath}\{UID}\{TrailNum}.csv" or whatever it looks like after combining...
 
 	public void FixedUpdate() {
 		if(trialLogger != null) {
-			Debug.Log("WriteLog");
 			trialLogger.WriteLog();
 		}
 	}
 
-	public string GenerateExperimentID() {
-		long timeCodeMs = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
-
-		StringBuilder sb = new StringBuilder();
-
-		long right = timeCodeMs % 10; //I guess we are using longs now... (it doesn't matter because this happens infrequently)
-
-		while(timeCodeMs > 0) {
-			right = (int)(timeCodeMs % 10); //Gets the rightmost digit
-
-			//Apply cypher here (append to sb when done)
-
-			timeCodeMs /= 10; //Gets rid of the rightmost digit
-		}
-	}
-
-
     public void NewTrial() {
-		if(gameObject == null){ throw new MissingReferenceException("TransformSpy: TransformSpy must be attached to player camera for logging to occur."); }
-		if(trialLogger != null){ trialLogger.Dispose(); }
+        if(JSONSerializer.Instance.SceneNumber == 1) { CreateBasePath(); }
+        if(gameObject == null) { throw new MissingReferenceException("TransformSpy: TransformSpy must be attached to player camera for logging to occur."); }
+        if(trialLogger != null) { trialLogger.Dispose(); }
 
-		trialLogger = new TrialLogger(gameObject.transform, path);
-		trialLogger.Start();
+        Debug.Log(JSONSerializer.Instance.SceneNumber);
+        trialLogger = new TrialLogger(gameObject.transform, Path.Combine(basePath, $"{JSONSerializer.Instance.SceneNumber.ToString()}.csv"));
+        trialLogger.Start();
+    }
+
+    //Notes for future more cognizant self:
+    // System must account for current trial loop
+    // basepath is paht of expirement folder, which is insider expierments folder.
+
+    //Right now all files get overriden?
+    //Scene number is always 7, when testing logging reported that the scene would change to level n and 7 where n was seemingly random as expected, but then level 7 was logged every single time.
+    //Currently for some reason only one csv is created per expierment, and it is always titled 7.
+
+    private void CreateBasePath() {
+        basePath = Path.Combine(Application.persistentDataPath, "Experiments", GenerateExperimentID());
+        string directoryPath = Path.GetDirectoryName(basePath);
+
+        if(Directory.Exists(directoryPath)){
+            Directory.CreateDirectory(directoryPath);
+        }
+    }
+
+    public string GenerateExperimentID() {
+		long uuid = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+		long key = 482338932;
+
+		uuid = (uuid % 1000000007) ^ key;
+		return Base36(uuid);
 	}
+
+    private string Base36(long num) {
+        //Takes number and converts it into base 36, the same as any other base.
+
+        const string chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        StringBuilder sb = new StringBuilder();
+
+        bool isNeg = num < 0;
+        num = Math.Abs(num);
+
+        //do while
+        do {
+            sb.Append(chars[(int)(num % 36)]);
+            num /= 36;
+        } while(num > 0);
+
+        string final = sb.ToString().Reverse<char>().ToString();
+        return isNeg ? "-" + sb.ToString() : sb.ToString();
+    }
 }
