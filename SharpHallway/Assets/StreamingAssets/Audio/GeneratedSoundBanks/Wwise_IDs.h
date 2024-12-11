79a98092f7e29287d4114f8b914be7986cfa8d77
Niftalemm <niftalem.kassa@mnsu.edu>
2024-12-10 22:51:51 -0600

    Merging Wwise with Logger

---
+++ SharpHallway/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,65 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_FOOTSTEPS = 3854155799U;
+        static const AkUniqueID PLAY_MUTEDTHUD3 = 716284631U;
+        static const AkUniqueID PLAY_OUCH = 1979731779U;
+        static const AkUniqueID PLAY_PUSHING_GROCERY_CART_63821 = 131492894U;
+        static const AkUniqueID PLAY_STEP1 = 2600469397U;
+        static const AkUniqueID PLAY_STEP2 = 2600469398U;
+        static const AkUniqueID PLAY_STEP3 = 2600469399U;
+        static const AkUniqueID PLAY_STOMP = 2753042117U;
+        static const AkUniqueID PLAY_WALLBUMP2 = 3126293924U;
+        static const AkUniqueID PLAY_WALLTHUD = 2955529881U;
+    } // namespace EVENTS
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID FOOTSTEPPAN = 1124109716U;
+        static const AkUniqueID FOOTSTEPREVERB = 60600791U;
+        static const AkUniqueID FRCOLLISIONPAN = 510951580U;
+        static const AkUniqueID FRONTREARPAN = 1716080809U;
+        static const AkUniqueID LRCOLLISIONPAN = 3291598030U;
+        static const AkUniqueID OBSTACLEDISTANCE = 1961994583U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID COLLISIONBUS = 1993163527U;
+        static const AkUniqueID FOOTSTEPBUS = 455962323U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID REVERB = 348963605U;
+    } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID HALLREVERB = 2904300592U;
+    } // namespace AUX_BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
